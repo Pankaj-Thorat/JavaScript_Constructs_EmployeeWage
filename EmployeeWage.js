6b0fab37f6d8155368886dd836e18ce8617a5638
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-06-06 11:01:39 +0530

    [Pankaj] Ability to calculate Daily Employee by using Function.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,19 +3,23 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const PER_HR_WAGE = 20;
-let empHrs = 0;
-let empInput = Math.floor(Math.random()*10)%3;
-switch(empInput)
+function getWorkingHours(empInput)
 {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HRS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HRS;
-        break;
-    default:
-        empHrs = 0;
+    let empHrs = 0 ;       
+    switch(empInput)
+    {
+        case IS_PART_TIME:
+            empHrs = PART_TIME_HRS;
+            break;
+        case IS_FULL_TIME:
+            empHrs = FULL_TIME_HRS;
+            break;
+        default:
+            empHrs = 0;
+    }
+    return empHrs;
 }
-
+let empInput = Math.floor(Math.random()*10)%3;
+empHrs = getWorkingHours(empInput);
 let empWage = empHrs * PER_HR_WAGE;
 console.log("Employee Wage is : " + empWage);
