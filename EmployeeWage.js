840c26d797a6b79096e6f36c9d43698afb0d37db
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-06-06 10:29:44 +0530

    [Pankaj] Ability to calculate Daily Employee.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,21 @@
-const IS_PRESENT = 1;
-let empInput = Math.floor(Math.random()*10)%2;
-if (empInput == IS_PRESENT)
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HRS = 4;
+const FULL_TIME_HRS = 8;
+const PER_HR_WAGE = 20;
+let empHrs = 0;
+let empInput = Math.floor(Math.random()*10)%3;
+switch(empInput)
 {
-    console.log(" Employee is Present. "); 
-    return;
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HRS;
+        break;
+    default:
+        empHrs = 0;
 }
-else
-{
-    console.log(" Employee is Absent. ");
-}
+
+let empWage = empHrs * PER_HR_WAGE;
+console.log("Employee Wage is : " + empWage);
