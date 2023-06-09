522ed1a10862a2b6493b57754d0fa26a9932036a
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-06-09 17:15:35 +0530

    Added monthly wage calculation

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,6 +3,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HRS = 4;
 const FULL_TIME_HRS = 8;
 const PER_HR_WAGE = 20;
+const WORKING_DAYS = 20;
+
 function getWorkingHours(empInput)
 {
     let empHrs = 0 ;       
@@ -19,7 +21,10 @@ function getWorkingHours(empInput)
     }
     return empHrs;
 }
-let empInput = Math.floor(Math.random()*10)%3;
-empHrs = getWorkingHours(empInput);
-let empWage = empHrs * PER_HR_WAGE;
-console.log("Employee Wage is : " + empWage);
+let totalEmpHrs = 0;
+for(let day = 0; day<WORKING_DAYS; day++){
+    let empInput = Math.floor(Math.random()*10)%3;
+    totalEmpHrs += getWorkingHours(empInput);
+}
+let empWage = totalEmpHrs * PER_HR_WAGE;
+console.log("Total Hrs = "+totalEmpHrs +" Employee Wage is : " + empWage);
